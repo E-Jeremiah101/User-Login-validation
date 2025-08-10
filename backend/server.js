2c9dd740ca71cf5a74712e722af3ede17eff62c8
Jeremy <enofejeremiah87@gmail.com>
2025-08-10 10:40:19 +0100

    frontandbackend changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import { connectToDB } from "./config/db.js";
 import dotenv from "dotenv";
 import User from "./models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -33,10 +35,39 @@ app.post("/api/signup", async (req, res) => {
        const usernameExists = await User.findOne({username});
 
        if(usernameExists){
-        return res.status(400).json({messsage: "Username is taken"})
-       }
-    }catch (error){
+        return res.status(400).json({messsage: "Username is taken, try another name"})
+       };
+       
+      //encrypt user password
+       const hashedPassword = await bcryptjs.hash(password, 10);
 
+     //create user documet/details
+       const userDoc =  await User.create({
+        fullname,
+        username,
+        email,
+        password: hashedPassword,
+       })
+
+       //implement jwt
+       JWT_SECRET = jesusmylove;
+
+       if(userDoc){
+        const token = jwt.sign({id: userDoc._id}, process.env.JWT_SECRET, {
+            expiresIn: "5d"
+        });
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 100,
+        })
+       }
+
+       return res.status(200).json({user:userDoc, message: "User created successfully!"})
+    }catch (error){
+      res.status(400).json({message: error.message})
     }
 })
 
