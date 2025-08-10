75d5b26177de1bd0222c2679f59bbbcd1250f73f
Jeremy <enofejeremiah87@gmail.com>
2025-08-10 11:00:23 +0100

    frontend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = 5000;
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send("Hello World")
 });
 
 app.post("/api/signup", async (req, res) => {
